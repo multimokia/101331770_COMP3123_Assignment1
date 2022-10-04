abcb6fb04704cd913f9b5d6649e0383800f52ef1
multimokia <multimokia3@gmail.com>
2022-10-04 13:43:07 -0400

    Add https support with fallback to http

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 ## ENV_VARS:
 - `PORT` - The desired port to listen on (if not provided, 8000 is assumed)
 - `DB_URI` - **REQUIRED**, connection string to a mongodb database. **WILL ERROR IF NOT PROVIDED**
+- `SSL_CERT_FP`- Path to an SSL cert file. If not present (or file isn't found), http will be used instead of https
+- `SSL_KEY_FP` - Path to an SSL key file. If not present (or file isn't found), http will be used instead of https
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+import fs from "fs";
+import https from "https";
 import { connect } from "mongoose";
 
 // Import routers
@@ -14,19 +16,57 @@ app.use("/api/emp/employees", employeeRouter);
 
 // Parse conf
 dotenv.config();
+// Get hosting/connection info
 const PORT = process.env.PORT || 8000;
 const DB_URI = process.env.DB_URI;
+
+// Check if we're using SSL
+const KEY_FILE = process.env.SSL_KEY_FP;
+const CERT_FILE = process.env.SSL_CERT_FP;
+
+let is_https = true;
+let key: Buffer | undefined, cert: Buffer | undefined = undefined;
 
 if (DB_URI === undefined) {
     throw new Error("No database URL supplied to DB_URI");
 }
 
+
+if (KEY_FILE === undefined || CERT_FILE === undefined) {
+    console.log("No SSL keys/certs found, using http.");
+    is_https = false;
+}
+
+else {
+    // Read in our keys
+    try {
+        key = fs.readFileSync(KEY_FILE);
+        cert = fs.readFileSync(CERT_FILE);
+    }
+
+    catch (err) {
+        console.error(`Failed to read key files: ${err}`);
+        is_https = false;
+    }
+}
+
+
 // Connect the db
 connect(DB_URI)
     .then(() => {
-        // Finally, begin listening once the db has been connected
-        app.listen(PORT, () => {
-            console.log(`App listening on port ${PORT}`);
-        });
+        if (is_https) {
+            // Finally, begin listening once the db has been connected
+            const server = https.createServer({key , cert }, app);
+
+            server.listen(PORT, () => {
+                console.log(`App listening on port https://localhost:${PORT}/`);
+            });
+        }
+
+        else {
+            app.listen(PORT, () => {
+                console.log(`App listening on port http://localhost:${PORT}/`);
+            });
+        }
     })
     .catch(console.error);
