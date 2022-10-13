be4f81cf53a6cd9e7faa69e5e52b03fa1cca5b5a
multimokia <multimokia3@gmail.com>
2022-10-12 21:14:47 -0400

    whoops

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,54 +1,43 @@
 import { json } from "express";
 import { Router } from "express";
 import { validateMissingProperties } from "../helpers/propertyvalidators.js";
-import { badRequest, respond } from "../helpers/responses.js";
-import { createEmployee, deleteEmployee, getAllEmployees, getEmployee, updateEmployee } from "../services/employeeserviceprovider.js";
+import {
+    badRequest,
+    respond
+} from "../helpers/responses.js";
+import { loginUser, registerUser } from "../services/userserviceprovider.js";
 
 const router = Router();
 
-router.route("/")
-    .get((req, res) => {
-        getAllEmployees()
-            .then(payload => respond(res, payload))
-            .catch(payload => respond(res, payload));
-    })
+router.route("/signup")
     .post(json(), (req, res) => {
-        const propValidationResult = validateMissingProperties(
+        const propertyValidationResult = validateMissingProperties(
             req.body,
-            ["first_name", "last_name", "email", "salary"]
+            ["username", "password", "email"]
         );
 
-        if (!propValidationResult.status) {
-            return respond(res, badRequest(propValidationResult));
+        if (!propertyValidationResult.status) {
+            return respond(res, badRequest(propertyValidationResult));
         }
 
-        // Now, see if we have an entry with this email already (since email addresses are unique)
-        createEmployee({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            email: req.body.email,
-            salary: req.body.salary,
-            gender: req.body.gender ?? null
-        })
+        // Now, see if we have an entry with this username already
+        registerUser(req.body)
             .then(payload => respond(res, payload))
             .catch(payload => respond(res, payload));
     });
 
-router.route("/:id")
-    .get((req, res) => {
-        getEmployee(req.params.id)
-            .then(payload => respond(res, payload))
-            .catch(payload => respond(res, payload));
-    })
-    .put(json(), (req, res) => {
-        // NOTE: we explicitly pass an object for update as a means of
-        // Ensuring no additional properties get added
-        updateEmployee(req.params.id, req.body)
-            .then(payload => respond(res, payload))
-            .catch(payload => respond(res, payload));
-    })
-    .delete((req, res) => {
-        deleteEmployee(req.params.id)
+router.route("/login")
+    .post(json(), (req, res) => {
+        const propertyValidationResult = validateMissingProperties(
+            req.body,
+            ["username", "password"]
+        );
+
+        if (!propertyValidationResult.status) {
+            return respond(res, badRequest(propertyValidationResult));
+        }
+
+        loginUser(req.body.username, req.body.password)
             .then(payload => respond(res, payload))
             .catch(payload => respond(res, payload));
     });
